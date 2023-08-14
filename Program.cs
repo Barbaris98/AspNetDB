fd6a499c20dfc4b89da2772c06c9dbb2a72ff12e
Dmitry Grigorev <d-d12@mail.ru>
2023-08-14 22:02:29 +0400

    1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using AspNetDB;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// получаем строку подключения из файла конфигурации
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// добавляем контекст ApplicationContext в качестве сервиса в приложение
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// получение данных
+app.MapGet("/", (ApplicationContext db) => db.Users.ToList());
 
 app.Run();
