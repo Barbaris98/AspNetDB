34e3c1a799d33e10eb581f8f0634083e43f4832c
Dmitry Grigorev <d-d12@mail.ru>
2023-08-14 21:07:09 +0400

    Добавьте файлы проекта.

---
+++ UserClass.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspNetDB
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = ""; // имя пользователя
+        public int Age { get; set; } // возраст пользователя
+    }
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated(); //создаём БД при первом обращении
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Name = "Tom", Age = 37 },
+                new User { Id = 2, Name = "Bob", Age = 41 },
+                new User { Id = 3, Name = "Sam", Age = 24 }
+                );
+        }
+    }
+
+
+}
